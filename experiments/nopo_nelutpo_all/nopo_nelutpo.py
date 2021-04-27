8fb3e6651fd9b7d6668def2d2aa6a67d436ad07c
Lassebk45 <lkar18@student.aau.dk>
2021-04-27 09:51:09 +0200

    organized test results, etc.

---
+++ experiments/nopo_nelutpo_all/nopo_nelutpo.py
@@ -0,0 +1,121 @@
+#! /usr/bin/env python
+
+import os
+
+from pathlib import Path
+
+import project
+
+from lab import tools
+from lab.environments import LocalEnvironment
+from downward.experiment import FastDownwardExperiment
+from downward.reports.absolute import AbsoluteReport
+from downward.reports.scatter import ScatterPlotReport
+
+
+env = LocalEnvironment(processes=1)
+exp = FastDownwardExperiment(environment=env)
+
+# Build
+
+exp.add_step("build", exp.build)
+
+# Run
+
+exp.add_step("start", exp.start_runs)
+
+# Search algorithms
+
+REPO = project.get_repo_base()
+REV = "main"
+
+CONFIGS = [
+    (f"{index:02d}-{h_nick}", ["--search", f"astar(merge_and_shrink(op_mutex=op_mutex({h}), stop_early=true))"])
+    for index, (h_nick, h) in enumerate(
+        [
+            ("NeLUTPO", "use_previous_ops=NeLUTPO, reachability_strategy=goal"),
+            ("NoPO", "use_previous_ops=NoPO, reachability_strategy=goal"),
+        ],
+        start=1,
+    )
+]
+
+BUILD_OPTIONS = []
+DRIVER_OPTIONS = ['--validate', '--overall-time-limit', '30m', '--overall-memory-limit', '9000M']
+
+for config_nick, config in CONFIGS:
+    exp.add_algorithm(
+        config_nick,
+        REPO,
+        REV,
+        config,
+        build_options=BUILD_OPTIONS,
+        driver_options=DRIVER_OPTIONS,
+    )
+
+#Domains and problems. must use environment variables or have benchmarks and downward REPO in same folder
+
+#Run on entire strips suite
+SUITE = ["agricola-opt18-strips", "airport", "barman-opt11-strips", "barman-opt14-strips", "blocks", "childsnack-opt14-strips", "data-network-opt18-strips", "depot", "driverlog", "elevators-opt08-strips", "elevators-opt11-strips", "floortile-opt11-strips", "floortile-opt14-strips", "freecell", "ged-opt14-strips", "grid", "gripper", "hiking-opt14-strips", "logistics00", "logistics98", "miconic", "movie", "mprime", "mystery", "nomystery-opt11-strips", "openstacks-opt08-strips", "openstacks-opt11-strips", "openstacks-opt14-strips", "openstacks-strips", "organic-synthesis-opt18-strips", "organic-synthesis-split-opt18-strips", "parcprinter-08-strips", "parcprinter-opt11-strips", "parking-opt11-strips", "parking-opt14-strips", "pathways", "pegsol-08-strips", "pegsol-opt11-strips", "petri-net-alignment-opt18-strips", "pipesworld-notankage", "pipesworld-tankage", "psr-small", "rovers", "satellite", "scanalyzer-08-strips", "scanalyzer-opt11-strips", "snake-opt18-strips", "sokoban-opt08-strips", "sokoban-opt11-strips", "spider-opt18-strips", "storage", "termes-opt18-strips", "tetris-opt14-strips", "tidybot-opt11-strips", "tidybot-opt14-strips", "tpp", "transport-opt08-strips", "transport-opt11-strips", "transport-opt14-strips", "trucks-strips", "visitall-opt11-strips", "visitall-opt14-strips", "woodworking-opt08-strips", "woodworking-opt11-strips", "zenotravel"]
+
+
+exp.add_suite("../../../benchmarks", SUITE)
+#exp.add_suite("../../../benchmarks", ["freecell:p01.pddl"])
+
+
+# If using environment variables
+
+# BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+# exp.add_suite(BENCHMARKS_DIR, SUITE)
+
+
+
+# Output fetching
+
+exp.add_fetcher(name="fetch")
+
+# Parsers for fetching
+
+DIR = Path(__file__).resolve().parent
+
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
+exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+exp.add_parser(exp.PLANNER_PARSER)
+exp.add_parser(DIR / "parser.py")
+
+# Absolute HTML Report
+
+ATTRIBUTES = [
+    "operator_mutex_time",
+    "operator_mutexes_num",
+    #project.EVALUATIONS_PER_TIME,
+]
+
+report = AbsoluteReport(attributes=ATTRIBUTES)
+
+name = "test-abs"
+outfile = f"{name}.{report.output_format}"
+
+exp.add_report(report, name=name, outfile=outfile)
+
+# ScatterPlot Report
+
+# report = ScatterPlotReport(attributes = "foo")
+
+# suffix = "-rel" if project.RELATIVE else ""
+# for algo1, algo2 in pairs:
+#     for attr in attributes:
+#         exp.add_report(
+#             project.ScatterPlotReport(
+#                 relative=project.RELATIVE,
+#                 get_category=None if project.TEX else lambda run1, run2: run1["domain"],
+#                 attributes=[attr],
+#                 filter_algorithm=[algo1, algo2],
+#                 filter=[project.add_evaluations_per_time],
+#                 format="tex" if project.TEX else "png",
+#             ),
+#             name=f"{exp.name}-{algo1}-vs-{algo2}-{attr}{suffix}",
+#         )
+
+exp.run_steps()
